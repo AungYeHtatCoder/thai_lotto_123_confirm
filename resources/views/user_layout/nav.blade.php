072d92b6935a3c4097e21388edcd9ac98133af4e
ThaeSuAung20 <thaesuaung552000@gmail.com>
2023-11-26 00:38:36 +0630

    change ui

--- resources/views/user_layout/nav.blade.php
+++ resources/views/user_layout/nav.blade.php
@@ -1,19 +1,19 @@
 <div class="container-fluid fixed-top">
- <div class="row">
-  <div class="col-lg-4 col-md-6 offset-lg-4 offset-md-3 navs">
-   <div class="px-3 py-3">
-    <div class="d-flex justify-content-between">
-        <span>
-            <a class="material-icons text-white" href="{{ url()->previous() }}">arrow_back</a>
-        </span>
-        <h5 class="mx-auto mt-1">
-            <a href="{{ url('/') }}" class="text-white">Thailotto123</a>
-        </h5>
-     <span class="mt-1">
-      <a class="material-icons text-white" href="{{ url('/') }}">refresh</a>
-     </span>
+    <div class="row">
+        <div class="col-lg-4 col-md-6 offset-lg-4 offset-md-3 navs">
+            <div class="px-3 py-3">
+                <div class="d-flex justify-content-between">
+                    <span>
+                        <button class="material-icons btn btn-sm text-white" onclick="goBack()">arrow_back</button>
+                    </span>
+                    <h5 class="mx-auto mt-1">
+                        <a href="{{ url('/') }}" class="text-white">Thailotto123</a>
+                    </h5>
+                    <span class="mt-1">
+                        <button class="material-icons btn btn-sm text-white" onclick="refreshPage()">refresh</button>
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
-   </div>
-  </div>
- </div>
 </div>
