f59f40d0115ec70a0e54a00fa5fc5924ea3dab38
Thein Thein Soe <theintheinsoe@xyberplanet.com>
2023-11-25 09:38:37 +0630

    submit button fix

--- database/seeders/TwoDigitsTableSeeder.php
+++ database/seeders/TwoDigitsTableSeeder.php
@@ -13,7 +13,7 @@ class TwoDigitsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 1; $i < 100; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             DB::table('two_digits')->insert([
                 'two_digit' => str_pad($i, 2, '0', STR_PAD_LEFT),
                 'created_at' => now(),
