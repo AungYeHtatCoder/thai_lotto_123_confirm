680766ed460c497b084f4081d3500a0bed033ada
Wai Linn Kyaw <108474411+wlktech@users.noreply.github.com>
2023-12-29 04:09:10 +0630

    login register profile api added

---
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthApi\LoginRequest;
+use App\Http\Requests\AuthApi\RegisterRequest;
+use App\Models\User;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+    public function login(LoginRequest $request)
+    {
+        $request->validated($request->all());
+        $credentials = $request->only('country_code', 'phone', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = User::where('phone', $request->phone)->first();
+            return $this->success([
+                'user' => $user,
+                'token' => $user->createToken('Api Token of '. $user->name)->plainTextToken
+            ]);
+        } else {
+            return $this->error("", "Credentials do not match!", 401);
+        }
+    }
+
+    public function register(RegisterRequest $request){
+        $request->validated($request->all());
+
+        $user = User::create([
+            'name' => $request->name,
+            'country_code' => $request->country_code,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of '.$user->name)->plainTextToken
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::user()->currentAccessToken()->delete(); 
+        return $this->success([
+            'message' => 'Logged out successfully'
+        ]);
+    }
+    
+    public function profile()
+    {
+        return $this->success(Auth::user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//publish routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+//protected routes
+Route::group(["middleware" => ['auth:sanctum']], function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', [AuthController::class, 'profile']);
+    
 });
