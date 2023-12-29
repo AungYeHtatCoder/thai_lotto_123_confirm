a9fc4233f0d57d653dfb162627b35d230245dbf3
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2023-12-29 11:38:44 +0630

    change place

--- routes/two_d_play.php
+++ routes/two_d_play.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\WithDrawController;
+use App\Http\Controllers\User\UserWalletController;
 
 Route::group(['prefix' => 'user', 'as' => 'user.', 'namespace' => 'App\Http\Controllers\User', 'middleware' => ['auth']], function () {
 
