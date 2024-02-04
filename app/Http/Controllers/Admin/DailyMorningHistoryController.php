260fa221af9f3164812d5e25624d32813ea6341a
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-02-04 09:50:30 +0630

    team

--- app/Http/Controllers/Admin/DailyMorningHistoryController.php
+++ app/Http/Controllers/Admin/DailyMorningHistoryController.php
@@ -22,6 +22,7 @@ class DailyMorningHistoryController extends Controller
     $twoDigits = DB::table('lottery_two_digit_pivot')
         ->join('two_digits', 'lottery_two_digit_pivot.two_digit_id', '=', 'two_digits.id')
         ->whereBetween('lottery_two_digit_pivot.created_at', [$startTime, $endTime])
+        ->where('lottery_two_digit_pivot.currency', 'mmk')
         ->select('two_digits.two_digit', 'lottery_two_digit_pivot.sub_amount', 'lottery_two_digit_pivot.prize_sent', 'lottery_two_digit_pivot.currency', 'lottery_two_digit_pivot.created_at') // Select the columns you need
         ->get();
 
@@ -29,22 +30,40 @@ class DailyMorningHistoryController extends Controller
         $totalSubAmount = $twoDigits->sum('sub_amount');
         $twod_limits = TwoDLimit::orderBy('id', 'desc')->first();
 
+        $startTime_baht = Carbon::today()->setHour(5)->setMinute(0); // Example: today at 2 PM
+        $endTime_baht = Carbon::today()->setHour(9)->setMinute(30); // Example: today at 4 PM
+        //return $startTime;
+        // Fetch the two digits within the specified time range
+        $twoDigits_baht = DB::table('lottery_two_digit_pivot')
+        ->join('two_digits', 'lottery_two_digit_pivot.two_digit_id', '=', 'two_digits.id')
+        ->whereBetween('lottery_two_digit_pivot.created_at', [$startTime_baht, $endTime_baht])
+        ->where('lottery_two_digit_pivot.currency', 'baht') // Add this line
+        ->select('two_digits.two_digit', 'lottery_two_digit_pivot.sub_amount', 'lottery_two_digit_pivot.prize_sent', 'lottery_two_digit_pivot.currency', 'lottery_two_digit_pivot.created_at') // Select the columns you need
+        ->get();
+
+        // Calculate the total sum of sub_amount
+        $totalSubAmount_baht = $twoDigits_baht->sum('sub_amount');
+        $twod_limits_baht = TwoDLimit::orderBy('id', 'desc')->first();
+
     
         return view('admin.two_d.daily-early-morning_history', [
            'displayTwoDigits' => $twoDigits,
             'totalSubAmount' => $totalSubAmount,
             'twod_limits' => $twod_limits,
+            'displayTwoDigits_baht' => $twoDigits_baht,
+            'totalSubAmount_baht' => $totalSubAmount_baht,
+            'twod_limits_baht' => $twod_limits_baht,
             
         ]);
     }
 
     public function TwodDailyEarlyMorningBathHistory()
     {
-         $startTime = Carbon::today()->setHour(5)->setMinute(0); // Example: today at 2 PM
-        $endTime = Carbon::today()->setHour(12)->setMinute(30); // Example: today at 4 PM
+        $startTime = Carbon::today()->setHour(5)->setMinute(0); // Example: today at 2 PM
+        $endTime = Carbon::today()->setHour(9)->setMinute(30); // Example: today at 4 PM
         //return $startTime;
-    // Fetch the two digits within the specified time range
-    $twoDigits = DB::table('lottery_two_digit_pivot')
+        // Fetch the two digits within the specified time range
+        $twoDigits = DB::table('lottery_two_digit_pivot')
         ->join('two_digits', 'lottery_two_digit_pivot.two_digit_id', '=', 'two_digits.id')
         ->whereBetween('lottery_two_digit_pivot.created_at', [$startTime, $endTime])
         ->where('lottery_two_digit_pivot.currency', 'baht') // Add this line
