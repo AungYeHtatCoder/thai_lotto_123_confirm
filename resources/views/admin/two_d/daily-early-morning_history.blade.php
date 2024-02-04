9ebdd36337088480b0d2dbacf43dfb61f95e4fe2
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-02-04 08:26:10 +0630

    history updates

--- resources/views/admin/two_d/daily-early-morning_history.blade.php
+++ resources/views/admin/two_d/daily-early-morning_history.blade.php
@@ -54,7 +54,8 @@
                        
                         <div class="ms-auto my-auto mt-lg-0 mt-4">
                             <div class="ms-auto my-auto">
-                                
+                                 <a href="{{ url('/admin/twod-daily-early-morning-history') }}"
+                                    class="btn bg-gradient-primary btn-sm mb-0"> > &nbsp; 9:30 စာရင်း ပေါင်းချုပ်</a>
                                 <button class="btn btn-outline-primary btn-sm export mb-0 mt-sm-0 mt-1" data-type="csv"
                                     type="button" name="button">Export</button>
                             </div>
