ee0d70bda70aaf705dd70253c0118e6d6b3492f6
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-02-07 00:59:57 +0630

    cors policy issue

--- config/cors.php
+++ config/cors.php
@@ -14,19 +14,29 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    // 'allowed_methods' => ['*'],
+
+    // 'allowed_origins' => ['*'],
+
+    // 'allowed_origins_patterns' => [],
+
+    // 'allowed_headers' => ['*'],
+
+    // 'exposed_headers' => [],
+
+    // 'max_age' => 0,
+
+    // 'supports_credentials' => false,
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['https://app.thailotto123.net'],
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['Content-Type', 'X-Requested-With'],
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
+
 ];
